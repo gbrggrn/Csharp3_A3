a069017b1edd6c275dc8402d21145a029211003d
gbrggrn <gustberg107@student.kau.se>
2025-10-22 14:19:26 +0200

    AppointmentsController added. Appointments.Index wired to controller. Controller wired to services.

---
+++ Controllers/AppointmentsController.cs
@@ -0,0 +1,27 @@
+﻿using Csharp3_A3.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Csharp3_A3.Controllers
+{
+	public class AppointmentController : Controller
+	{
+		//Services
+		private readonly AccountService _accountService;
+		private readonly AppointmentService _appointmentService;
+		private readonly PatientService _patientService;
+		private readonly StaffService _staffService;
+
+		public AppointmentController(AccountService accountService, AppointmentService appointmentService, PatientService patientService, StaffService staffService)
+		{
+			_accountService = accountService;
+			_appointmentService = appointmentService;
+			_patientService = patientService;
+			_staffService = staffService;
+		}
+
+		public IActionResult Index()
+		{
+			return View();
+		}
+	}
+}
---
+++ ViewModels/AppointmentsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Csharp3_A3.Models;
+
+namespace Csharp3_A3.ViewModels
+{
+	public class AppointmentsViewModel
+	{
+		public Staff? Staff { get; set; }
+		public Patient? Patient { get; set; }
+		public List<Appointment> Appointments { get; set; }
+	}
+}
