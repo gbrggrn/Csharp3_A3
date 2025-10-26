2125f46146ea617092a8f5d89274b56f2c031759
gbrggrn <gustberg107@student.kau.se>
2025-10-26 10:56:22 +0100

    StaffDashboard migrated to MVC pattern, wired through controller - pagemodel removed. StaffDashboardViewModel now serves the StaffDashboard page.

---
+++ Controllers/StaffController.cs
@@ -0,0 +1,57 @@
+﻿using Csharp3_A3.Models;
+using Csharp3_A3.Services;
+using Csharp3_A3.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Csharp3_A3.Controllers
+{
+	public class StaffController : Controller
+	{
+		//Services
+		private readonly StaffService _staffService;
+		private readonly PatientService _patientService;
+		private readonly AccountService _accountService;
+
+		public StaffController(StaffService staffService, PatientService patientService, AccountService accountService)
+		{
+			_staffService = staffService;
+			_patientService = patientService;
+			_accountService = accountService;
+		}
+
+		public async Task<IActionResult> StaffDashboard(int? id, string? tab)
+		{
+			var staffDashboardViewModel = new StaffDashboardViewModel();
+
+			string activeTab = tab ?? "tab1";
+			staffDashboardViewModel.ActiveTab = activeTab;
+
+			var username = User.Identity?.Name;
+			var user = await _accountService.GetByUserNameAsync(username);
+
+			if (user?.StaffId != null)
+			{
+				var currentStaff = await _staffService.GetByIdAsync(user.StaffId.Value);
+				var patients = await _patientService.GetAllAsync();
+				var appointments = await _staffService.GetAllAppointmentsByIdAsync(user.StaffId.Value);
+
+				staffDashboardViewModel.CurrentStaff = currentStaff;
+				staffDashboardViewModel.Patients = patients;
+				staffDashboardViewModel.Appointments = appointments;
+			}
+
+			if (id.HasValue)
+			{
+				var selectedPatient = await _patientService.GetByIdAsync(id.Value);
+				staffDashboardViewModel.SelectedPatient = selectedPatient;
+				if (selectedPatient != null)
+				{
+					var medicalHistory = await _patientService.GetMedicalHistoryByPatientIdAsync(selectedPatient.Id);
+					staffDashboardViewModel.MedicalHistory = medicalHistory;
+				}
+			}
+
+			return View(staffDashboardViewModel);
+		}
+	}
+}
---
+++ ViewModels/StaffDashboardViewModel.cs
@@ -0,0 +1,14 @@
+﻿using Csharp3_A3.Models;
+
+namespace Csharp3_A3.ViewModels
+{
+	public class StaffDashboardViewModel
+	{
+		public Staff? CurrentStaff { get; set; }
+		public List<Patient> Patients { get; set; } = [];
+		public List<Appointment> Appointments { get; set; } = [];
+		public List<MedicalHistory> MedicalHistory { get; set; } = [];
+		public Patient? SelectedPatient { get; set; }
+		public string ActiveTab { get; set; } = "tab1";
+	}
+}
